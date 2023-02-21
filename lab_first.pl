b45385419a8faa1677ac759287a9276908660ea8
Sagar <86335892+Th-Sagar@users.noreply.github.com>
2023-02-21 18:10:53 +0545

    new

---
+++ lab_first.pl
@@ -0,0 +1,16 @@
+clauses:
+start.
+male(ram).
+male(hari).
+male(gopal).
+female(sita).
+female(rita).
+likes(ram,icecream).
+likes(sita,icecream).
+likes(ram,sita).
+sum(X,Y):-W is X+Y,write(W).
+max(X,Y):-X>Y,write(X).
+max(X,Y):-X<Y,write(Y).
+min(X,Y):-X>Y,write(Y).
+min(X,Y):-X<Y,write(X).
+
