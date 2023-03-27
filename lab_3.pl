5503c805280ade8d47a75debade616726fa0a184
Sagar <86335892+Th-Sagar@users.noreply.github.com>
2023-03-27 20:37:48 +0545

    new

--- lab_3.pl
+++ lab_3.pl
@@ -25,3 +25,12 @@ addtoend(H,[],[H]).
 addtoend(X,[H|T],[H|T1]):-addtoend(X,T,T1).
 reversex([],[]).
 reversex([H|T],Y):- reversex(T,T1),addtoend(H,T1,Y).
+
+water(P,_):-P>4 ,write("P capacity is full").
+water(_,Q):-Q>3,write("Q capacity is full").
+water(P,Q):-P is 4,Q is 3,P1 is 0, Q1 is 3 ,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,water(P1,Q1).
+water(P,Q):-P is 0,Q is 3,P1 is 3, Q1 is 0,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,water(P1,Q1).
+water(P,Q):-P is 3,Q is 0,P1 is 3, Q1 is 3,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,water(P1,Q1).
+water(P,Q):-P is 3,Q is 3,P1 is 4, Q1 is 2 ,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,water(P1,Q1).
+water(P,Q):-P is 4,Q is 2,P1 is 0, Q1 is 2 ,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,water(P1,Q1).
+water(P,Q):-P is 0,Q is 2,P1 is 2, Q1 is 0 ,write("jug1 ="),write(P1),write("jug2="),write(Q1),nl,write("solution reached").
